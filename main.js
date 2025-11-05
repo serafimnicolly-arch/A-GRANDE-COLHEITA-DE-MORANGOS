fe529a1a382af0b7ea2d02359bc7c3653dc72862
serafimnicolly-arch <serafim.nicolly@escola.pr.gov.br>
2025-11-05 09:53:19 -0300

    j

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const avanca =document.querySelectorAll('.btn-proximo')
 console.log(avanca)
 
 avanca.forEach(button =>{
- button.addEventListener('clik' , function () { 
+ button.addEventListener('click' , function () { 
     const atual = document.querySelector('.ativo')
  const proximoPasso = 'passo-' + this.getAttribute ('data-proximo')
 
